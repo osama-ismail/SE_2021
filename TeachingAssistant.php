c13dc375e2168bfd2a66f6cf1fb6554d8a8ddd41
Ahmad Shaer <54283555+AHMAD-KING@users.noreply.github.com>
2021-11-23 18:27:44 +0200

    Add files via upload

---
+++ TeachingAssistant.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>eTeaching</title>
+    <style>
+        body{background-color:#3d4c74}
+        a{ width: 25%}
+    </style>
+</head>
+<body>
+<div style=" background-color:white; width:100%">
+
+    <table style="width: 100%">
+        <tr style="width: 100%">
+            <td style="width: 40%">
+                <span> <pre> Welcome to eTeaching </pre> </span>
+            </td>
+
+            <td style="width: 10%">
+                <a href="" > Profile </a>
+            </td>
+            <td style="width: 10%">
+                <a href="" > Mycourses </a>
+            </td>
+            <td style="width: 10%">
+                <a href="" > Marks </a>
+            </td>
+            <td style="width: 10%" >
+                <a href="" > Logout </a>
+            </td>
+        </tr>
+    </table>
+
+    <br>
+
+    <center> <h1> eTeaching </h1>  </center>
+
+    <br>
+
+    <span> --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- </span>
+
+    <pre>
+         Announcements :
+            * info
+            * info
+            * info
+
+         Tutorials :
+            Some tips
+            Some tips
+
+
+         information about the site :
+            info
+            info
+            info
+     </pre>
+
+
+</div>
+</body>
+</html>
